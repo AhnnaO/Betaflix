a3951cc8e46d8efaa792a26f24a34c2c18ded23d
Ahnna <ahnnaostling@gmail.com>
2019-05-06 10:13:39 +0200

    added components for routing and links to categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SearchComponent } from './search/search.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { DetailsComponent } from './details/details.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoots = [
+  {path: 'details/:id', component: DetailsComponent},
+  {path: 'category/:id', component: CategoriesComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     SearchComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    DetailsComponent,
+    AboutComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoots)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
