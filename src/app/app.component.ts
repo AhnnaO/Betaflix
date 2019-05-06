1c0e54e1761fa472053ad90beb1da2b24d438c49
Ahnna <ahnnaostling@gmail.com>
2019-05-06 09:33:55 +0200

    seperated navigation to it's own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Betaflix';
-  tagLine = 'Ultimate retro movie experience';
   betaFlixImage = '../assets/images/betamax-photo-of-logo.jpg';
 }
